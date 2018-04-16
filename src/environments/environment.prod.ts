e953a7ecbcd485447aa7fbcc3b73a34335face69
sosa <w_sosa@live.com>
2018-04-15 20:20:47 -0600

    Firebase Configuracion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyACg1F69z9EZYGS_7sxhLg8YrTmQH0wUos",
+    authDomain: "loginapp-8625a.firebaseapp.com",
+    databaseURL: "https://loginapp-8625a.firebaseio.com",
+    projectId: "loginapp-8625a",
+    storageBucket: "loginapp-8625a.appspot.com",
+    messagingSenderId: "128974511883"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,15 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+    // Initialize Firebase
+  firebaseConfig: {
+    apiKey: "AIzaSyACg1F69z9EZYGS_7sxhLg8YrTmQH0wUos",
+    authDomain: "loginapp-8625a.firebaseapp.com",
+    databaseURL: "https://loginapp-8625a.firebaseio.com",
+    projectId: "loginapp-8625a",
+    storageBucket: "loginapp-8625a.appspot.com",
+    messagingSenderId: "128974511883"
+  }
 };
